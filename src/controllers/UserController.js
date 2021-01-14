74d0a3c8d9faf2cc49b0e02d4315d3e153e11173
GabrielCoffee9 <63719840+GabrielCoffee9@users.noreply.github.com>
2021-01-14 08:36:18 -0300

    Update :house_with_garden:
    Withdraw update

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -82,12 +82,13 @@ class UserController {
         if (username == undefined || receive_user == undefined || value == undefined || type == undefined) {
             console.log(username, receive_user, value, type)
             res.status(400).json({ error: "Some of the values in body is wrong" })
+            throw error
         }
 
 
         if (value < 0) {
             res.status(400).json({ error: "value should be positive for operation deposit" })
-
+            throw error
         }
         var sqlText =
             `SELECT id FROM users
@@ -124,7 +125,62 @@ class UserController {
         return res.json({ message: "Deposit successful" })
 
     }
-}
 
+    async Withdraw(req, res) {
+        const { username, receive_user, value, type } = req.body
+
+        if (username == undefined || receive_user == undefined || value == undefined || type == undefined) {
+            console.log(username, receive_user, value, type)
+            res.status(400).json({ error: "Some of the values in body is wrong" })
+            throw error
+        }
+
+
+        if (value > 0) {
+            res.status(400).json({ error: "value should be negative for operation Withdraw" })
+            throw error
+
+        }
+        var sqlText =
+            `SELECT id, balance FROM users
+        WHERE
+        name = '${username}' `
+
+        try {
+            var results = await conn.execute(sqlText)
+        }
+        catch (error) {
+            console.log(error.sqlMessage)
+            throw error
+        }
+        const user_results = results[0][0]
+        if (user_results.balance < (value * -1)) {
+            res.status(400).json({ error: "The value is more than the user have in the balance account" })
+            throw error
+        }
+
+        sqlText =
+            `UPDATE
+            users
+         SET
+            balance = balance - ${value * -1},
+            updated_at = NOW()
+         WHERE
+            id = ${user_results.id}`
+
+        console.log(sqlText)
+        try {
+            await conn.execute(sqlText)
+            await newTransaction(user_results.id, receive_user, undefined, value, 'withdraw', '')
+        }
+        catch (error) {
+            console.log(error.sqlMessage)
+            throw error
+        }
+        return res.json({ message: "Withdraw successful" })
+
+    }
+
+}
 module.exports = new UserController()
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,24 +4,13 @@ const connection = require('../database/connection')
 const routes = express.Router()
 const UserController = require('../controllers/UserController')
 
-
-
-routes.post('/createUser', UserController.createUser)
-
 routes.get('/', (req, res) => {
     res.json({ message: "Welcome to Coffee Bank!" })
 })
 
-
+routes.post('/createUser', UserController.createUser)
 routes.post('/deposit', UserController.Deposit)
-// routes.get('/', (req, res) => {
-//     return res.json({ message: "hello world" })
-// })
+routes.post('/withdraw', UserController.Withdraw)
 
-// routes.post('/createUser', UserController.createUser)
-
-// routes.post('/deposit', UserController.Deposit)
-
-// routes.post('/withdraw', UserController.Withdraw)
 
 module.exports = routes
